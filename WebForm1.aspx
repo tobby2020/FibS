860e63dc9e56667e973deb4fe2388176db368389
0213 <amtobby@hotmail.com>
2020-03-05 02:48:00 +0800

    加入專案檔案。

---
+++ WebForm1.aspx
@@ -0,0 +1,23 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="WebForm1.aspx.cs" Inherits="Fibs.WebForm1" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+    <title></title>
+</head>
+<body>
+        
+    <form id="form1" runat="server">
+        <asp:Label ID="Label1" runat="server" Text="輸入"></asp:Label>&nbsp;&nbsp;
+        
+        <asp:TextBox ID="TextBox1" runat="server" Text="" ></asp:TextBox>
+                <p>
+        <asp:Button ID="Button1" runat="server" Text="費式數列" OnClick="Button1_Click" />
+                <p>
+        <asp:Label ID="Label2" runat="server" Text="輸出"></asp:Label>
+    </form>
+        
+</body>
+</html>
---
+++ WebForm1.aspx.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Windows.Forms;
+
+namespace Fibs
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+        private System.DirectoryServices.DirectorySearcher directorySearcher1;
+
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            int a = 0;
+            int an = 0;
+
+            try
+            {
+                 a = int.Parse(TextBox1.Text);
+
+                for (int i = 0; i < a; i++)
+
+                {
+
+                    Console.Write(foo(a).ToString() + ",");
+                    an = foo(a);
+                }
+
+                Label2.Text = "第" + a + "數字為: " + Convert.ToString(an);
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("请输入数字");
+                Label2.Text = "請輸入數字";
+                TextBox1.Text = "";
+                a = 0;
+            }         
+            
+            
+        }
+
+        static int foo(int i)
+        {
+             if (i == 0 || i == 1)
+                return i;
+              else
+                return foo(i - 1) + foo(i - 2);
+        }
+
+       
+    }
+}
